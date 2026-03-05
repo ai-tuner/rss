e0e26ab5c5006d9baa4cc7a4e8742b963f0f6d6e
Banothu Naresh Naik <benareshnaik@gmail.com>
2026-03-05 18:49:16 +0530

    Update multi-rss.js

--- multi-rss.js
+++ multi-rss.js
@@ -8,10 +8,7 @@ const rssFeeds = [
     url: "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
     source: "ToI - Top Stories"
   },
- {
-url: "https://telanganatoday.com/feed",
-    source: "Telangana Today - Top Stories"
-  },
+ 
 {
 url: "https://www.oneindia.com/rss/feeds/news-india-fb.xml",
     source: "OneIndia - Top Stories"
